4ee587fb93a518bdf47bcea575ee45845213f86b
faiz-oussama <faizouss123@gmail.com>
2025-07-28 15:35:09 +0100

    project initialising

---
+++ testgen-ai/src/main/java/com/univade/testgen_ai/TestgenAiApplication.java
@@ -0,0 +1,13 @@
+package com.univade.testgen_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgenAiApplication.class, args);
+	}
+
+}
